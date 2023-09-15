9bed4e594388c492714f0e90510c144fb474bae8
traitimtrongvang <hutthuoclacohaichosuckhoe@outlook.com>
2023-09-16 00:49:49 +0700

    yeah

---
+++ Application/Domain/Entities/BaseEntity.cs
@@ -0,0 +1,13 @@
+﻿using Application.Domain.ValueObjects;
+
+namespace Application.Domain.Entities;
+
+public abstract class BaseEntity<TId> 
+    where TId : BaseGuidId
+{
+    public required TId Id { get; init; }
+
+    public required DateTime CreatedAt { get; init; }
+
+    public DateTime? UpdatedAt { get; set; } = default;
+}
